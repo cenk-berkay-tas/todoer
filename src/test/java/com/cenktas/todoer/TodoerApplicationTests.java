e9f37ef5a5a32346d80d9dfc88e83697bbbbd952
Cenk Berkay Taş <cenkbtas@gmail.com>
2025-08-26 09:57:39 +0300

    Add project files

---
+++ src/test/java/com/cenktas/todoer/TodoerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cenktas.todoer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
